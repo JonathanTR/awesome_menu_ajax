0fb49ca8bcc105e9685caf92dd84acb9de7b6327
bbeaird <apprentice@dbc18.local>
2013-11-04 14:26:59 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JtrNatMenu::Application.config.secret_token = 'b56aa89f67e2f7cef0cf1f0e8590eb936e51e73bed9ed49a8a02854655646a300e0daee6169b297dbf22193433a919d2dd64e950ce982b03783932bccf808928'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+JtrNatMenu::Application.config.session_store :cookie_store, key: '_jtr-nat-menu_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# JtrNatMenu::Application.config.session_store :active_record_store
